6a0bbe2ee4912ecbb9c0f335ce73aef3b266b1cb
Stoabrogga <stoabrogga@proton.me>
2023-12-17 00:18:41 +0100

    Adapt to sol@eaf4e41b4f6e19bc9cf83bf719c0203a3a195159

--- data/sql/db-world/morphsummon.sql
+++ data/sql/db-world/morphsummon.sql
@@ -13,9 +13,9 @@ SET @MENU_SORRY      := @MENU_HELLO + 1;
 SET @MENU_CHOICE     := @MENU_HELLO + 2;
 
 DELETE FROM `creature_template` WHERE `entry` = @ENTRY;
-INSERT INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `modelid1`, `modelid2`, `modelid3`, `modelid4`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `scale`, `rank`, `dmgschool`, `DamageModifier`, `BaseAttackTime`, `RangeAttackTime`, `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `resistance1`, `resistance2`, `resistance3`, `resistance4`, `resistance5`, `resistance6`, `spell1`, `spell2`, `spell3`, `spell4`, `spell5`, `spell6`, `spell7`, `spell8`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `InhabitType`, `HoverHeight`, `HealthModifier`, `ManaModifier`, `ArmorModifier`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `flags_extra`, `ScriptName`, `VerifiedBuild`)
+INSERT INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `modelid1`, `modelid2`, `modelid3`, `modelid4`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `scale`, `rank`, `dmgschool`, `DamageModifier`, `BaseAttackTime`, `RangeAttackTime`, `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `resistance1`, `resistance2`, `resistance3`, `resistance4`, `resistance5`, `resistance6`, `spell1`, `spell2`, `spell3`, `spell4`, `spell5`, `spell6`, `spell7`, `spell8`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `InhabitType`, `HoverHeight`, `HealthModifier`, `ManaModifier`, `ArmorModifier`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `flags_extra`, `ScriptName`)
 VALUES
-(@ENTRY,0,0,0,0,0,@MODELID,0,0,0,@NAME,@SUBNAME,NULL,0,80,80,2,35,1,1,1.14286,1,0,0,1,2000,2000,8,0,2048,0,0,0,0,0,0,7,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,'',0,3,1,1,1,1,0,0,1,0,2,@SCRIPTNAME,0);
+(@ENTRY,0,0,0,0,0,@MODELID,0,0,0,@NAME,@SUBNAME,NULL,0,80,80,2,35,1,1,1.14286,1,0,0,1,2000,2000,8,0,2048,0,0,0,0,0,0,7,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,'',0,3,1,1,1,1,0,0,1,0,2,@SCRIPTNAME);
 
 DELETE FROM `npc_text` WHERE `ID` IN (@NPC_TEXT_HELLO,@NPC_TEXT_SORRY,@NPC_TEXT_CHOICE);
 INSERT INTO `npc_text` (`ID`, `text0_0`)
@@ -25,12 +25,12 @@ VALUES
 (@NPC_TEXT_CHOICE, 'Please make your choice:');
 
 DELETE FROM `gossip_menu_option` WHERE `MenuID` IN (@MENU_HELLO,@MENU_SORRY,@MENU_CHOICE);
-INSERT INTO `gossip_menu_option` (`MenuID`, `OptionID`, `OptionIcon`, `OptionText`, `OptionBroadcastTextID`, `OptionType`, `OptionNpcFlag`, `ActionMenuID`, `ActionPoiID`, `BoxCoded`, `BoxMoney`, `BoxText`, `BoxBroadcastTextID`, `VerifiedBuild`)
+INSERT INTO `gossip_menu_option` (`MenuID`, `OptionID`, `OptionIcon`, `OptionText`, `OptionBroadcastTextID`, `OptionType`, `OptionNpcFlag`, `ActionMenuID`, `ActionPoiID`, `BoxCoded`, `BoxMoney`, `BoxText`, `BoxBroadcastTextID`)
 VALUES
-(@MENU_HELLO,0,0,'Choose polymorph',0,0,0,0,0,0,0,'',0,0),
-(@MENU_HELLO,1,0,'Choose Felguard weapon',0,0,0,0,0,0,0,'',0,0),
-(@MENU_HELLO,2,0,'Generate new name',0,0,0,0,0,0,0,'',0,0),
-(@MENU_SORRY,0,0,'Ah, nevermind.',0,0,0,0,0,0,0,'',0,0),
-(@MENU_CHOICE,0,0,'Back..',0,0,0,0,0,0,0,'',0,0),
-(@MENU_CHOICE,1,4,'Next..',0,0,0,0,0,0,0,'',0,0),
-(@MENU_CHOICE,2,4,'Previous..',0,0,0,0,0,0,0,'',0,0);
+(@MENU_HELLO,0,0,'Choose polymorph',0,0,0,0,0,0,0,'',0),
+(@MENU_HELLO,1,0,'Choose Felguard weapon',0,0,0,0,0,0,0,'',0),
+(@MENU_HELLO,2,0,'Generate new name',0,0,0,0,0,0,0,'',0),
+(@MENU_SORRY,0,0,'Ah, nevermind.',0,0,0,0,0,0,0,'',0),
+(@MENU_CHOICE,0,0,'Back..',0,0,0,0,0,0,0,'',0),
+(@MENU_CHOICE,1,4,'Next..',0,0,0,0,0,0,0,'',0),
+(@MENU_CHOICE,2,4,'Previous..',0,0,0,0,0,0,0,'',0);
